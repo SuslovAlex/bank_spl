e52ea8604c6322439f5a28d224f173f089c8c503
Alex Suslov <suslo.2001@mail.ru>
2022-03-10 19:17:37 +0500

    Firebase Adding

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bank-apl',
+    appId: '1:480816105742:web:678fa3b8f7b17fe026ef5f',
+    storageBucket: 'bank-apl.appspot.com',
+    apiKey: 'AIzaSyCieYRvWOY3G_poafawGbp3PxOWoSseXtI',
+    authDomain: 'bank-apl.firebaseapp.com',
+    messagingSenderId: '480816105742',
+    measurementId: 'G-7GDQZQ4109',
+  },
   production: false
 };
 
