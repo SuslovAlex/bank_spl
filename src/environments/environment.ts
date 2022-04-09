b7b6fb5d5b1a58b5a53a03da0ec5da625b62efc6
Alex Suslov <suslo.2001@mail.ru>
2022-04-09 15:23:48 +0500

    Fix bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,28 @@
+/* eslint-disable @typescript-eslint/typedef */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'bank-apl',
-    appId: '1:480816105742:web:678fa3b8f7b17fe026ef5f',
-    storageBucket: 'bank-apl.appspot.com',
-    apiKey: 'AIzaSyCieYRvWOY3G_poafawGbp3PxOWoSseXtI',
-    authDomain: 'bank-apl.firebaseapp.com',
-    messagingSenderId: '480816105742',
-    measurementId: 'G-7GDQZQ4109',
-  },
-  production: false
+    firebase: {
+        projectId: 'bank-apl',
+        appId: '1:480816105742:web:678fa3b8f7b17fe026ef5f',
+        storageBucket: 'bank-apl.appspot.com',
+        apiKey: 'AIzaSyCieYRvWOY3G_poafawGbp3PxOWoSseXtI',
+        authDomain: 'bank-apl.firebaseapp.com',
+        messagingSenderId: '480816105742',
+        measurementId: 'G-7GDQZQ4109',
+    },
+    production: false,
 };
 
 /*
  * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ * to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask`.
  *
- * This import should be commented out in production mode because it will have a negative impact
+ * This import should be commented out in production
+ * mode because it will have a negative impact
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
